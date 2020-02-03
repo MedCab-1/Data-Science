cba6bd64b9c4b3b92c5fbef754aacb75936718d0
James Luttringer <jamesluttringer18@gmail.com>
2020-02-03 18:59:57 -0500

    populated database, deployed on heroku

--- API/app.py
+++ API/app.py
@@ -1,5 +1,7 @@
 # Imports
 from flask import Flask, jsonify, request
+from decouple import config
+from .models import DB, Strain
 import basilica
 import numpy as np
 import pandas as pd
@@ -9,6 +11,10 @@ import pandas as pd
 def create_app():
     '''Create and configure an instance of the Flask application'''
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    DB.init_app(app)
 
     @app.route('/')
     def root():
--- API/models.py
+++ API/models.py
@@ -3,9 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 DB = SQLAlchemy()
 
 class Strain(DB.Model):
+    '''A simple representation of a cannabis strain'''
+
     id = DB.Column(DB.Integer, primary_key=True) 
-    strainName = DB.Column(DB.String(30), nullable=False)
-    Type = DB.Column(DB.String(6), nullable=False)
+    name = DB.Column(DB.String(30), nullable=False)
+    kind = DB.Column(DB.String(6), nullable=False)
     rating = DB.Column(DB.Float, nullable=False)
     effects = DB.Column(DB.Text, nullable=False)
     flavor = DB.Column(DB.Text, nullable=False)
