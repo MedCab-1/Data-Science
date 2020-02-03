0644eef4351d159bf7b3a829fccd5918a6268df7
James Luttringer <jamesluttringer18@gmail.com>
2020-02-03 14:32:28 -0500

    edit app

--- API/app.py
+++ API/app.py
@@ -1,25 +1,17 @@
 # Imports
-from flask import Flask, render_template, request
+from flask import Flask, jsonify, request
 import basilica
 import numpy as np
 import pandas as pd
 
-# from .models import DB, User
-# from .predict import predict_
 
 
 def create_app():
     '''Create and configure an instance of the Flask application'''
     app = Flask(__name__)
-    # app.config['SQLALCHEMY_DATABASE_URI'] = config('HEROKU_POSTGRESQL_COBALT_URL')
-    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    # DB.init_app(app)
 
     @app.route('/')
     def root():
-        # DB.create_all()
-        return render_template('base.html', title='Home', '''users=...''')
-
-    # app.route('/profile')
+        return "welcome to the api"
 
     return app
